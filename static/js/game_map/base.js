badb045df5b6b3d92adba05c8e2dc26589a87b96
lxl <718961185@qq.com>
2023-01-25 19:58:56 +0800

    写到canvas

---
+++ static/js/game_map/base.js
@@ -0,0 +1,11 @@
+import {
+    AcGameObject,
+} from '/static/js/ac_game_object/base.js';
+
+class GameMap extends AcGameObject {
+    constructor(root) {
+        super();
+        let $canvas = $('<canvas width="1280" height="720"></canvas> tabindex=0');
+
+    }
+}
